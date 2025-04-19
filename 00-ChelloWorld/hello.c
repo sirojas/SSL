bcd7151fc1787e33a83c2e31f2ef9f7f8782ac70
Simon Rojas <sirojas@frba.utn.edu.ar>
2025-04-19 14:59:23 -0300

    Update hello.c

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -3,7 +3,8 @@
 int main(){
     FILE *output = fopen("output.txt","w");
     printf("hola Simon Rojas");
-    fprintf(output,"hola Simon Rojas");
+    stdout = output;
+    printf("hola Simon Rojas");
     fclose(output);
     return 0;
 }
